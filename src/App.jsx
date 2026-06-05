b832077dc4481dfc3f9b722912fdc80ca47d9262
Hoorish <hoorishiqbal@gmail.com>
2026-06-06 01:02:29 +0500

    Created FallbackProject component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Sidebar from "./components/Sidebar.jsx";
 import Project from "./components/Project.jsx";
+import FallbackProject from "./components/FallbackProject.jsx";
 
 function App() {
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
         <Sidebar />
-        <Project /> 
+        <FallbackProject /> 
       </main>
     </>
   );
